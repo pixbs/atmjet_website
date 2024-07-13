143d1f3cc749c95701b530b355a1eaeb8924992b
pixbs <hey@pixbs.com>
2024-07-13 05:03:06 +0200

    🖋️ Update content for the homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,5 +2,5 @@ import {useTranslations} from 'next-intl';
  
 export default function HomePage() {
   const t = useTranslations('HomePage');
-  return <h1>{t('title')}</h1>;
+  return <h1>{t('why_us.cards.card1.title')}</h1>;
 }
