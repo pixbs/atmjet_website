d4beab6d928d5bfc02dc45432b221254bbecb462
pixbs <hey@pixbs.com>
2024-07-14 07:38:48 +0200

    🔢 chores: formatin counter and index

--- src/components/elements/counter.tsx
+++ src/components/elements/counter.tsx
@@ -1,69 +1,69 @@
 'use client'
-import React, { useEffect, useState } from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React, { useEffect, useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 interface CounterProps {
-  children: string;
-  className?: string;
+	children: string
+	className?: string
 }
 
-export function Counter({ children, className = "" }: CounterProps) {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
-  const [count, setCount] = useState<string>(children);
+export function Counter({ children, className = '' }: CounterProps) {
+	const controls = useAnimation()
+	const [ref, inView] = useInView()
+	const [count, setCount] = useState<string>(children)
 
-  useEffect(() => {
-    if (inView) {
-      controls.start({ opacity: 1, y: 0, transition: { duration: 0.5 } });
-      const duration = 800;
-      const incrementTime = 30;
+	useEffect(() => {
+		if (inView) {
+			controls.start({ opacity: 1, y: 0, transition: { duration: 0.5 } })
+			const duration = 800
+			const incrementTime = 30
 
-      const matches = children.match(/\d+([,.]\d+)?/g);
-      if (!matches) return;
+			const matches = children.match(/\d+([,.]\d+)?/g)
+			if (!matches) return
 
-      const numbers = matches.map((num) => parseFloat(num.replace(/,/g, '')));
-      const startNumbers = Array(numbers.length).fill(0);
+			const numbers = matches.map((num) => parseFloat(num.replace(/,/g, '')))
+			const startNumbers = Array(numbers.length).fill(0)
 
-      const step = (start: number[]) => {
-        const newStart = start.map((num, index) => {
-          const increment = numbers[index] / (duration / incrementTime);
-          return num + increment;
-        });
+			const step = (start: number[]) => {
+				const newStart = start.map((num, index) => {
+					const increment = numbers[index] / (duration / incrementTime)
+					return num + increment
+				})
 
-        let numIndex = 0;
-        const formattedCount = children.replace(/\d+([,.]\d+)?/g, () => {
-          const num = newStart[numIndex].toLocaleString(undefined, {
-            maximumFractionDigits: 0,
-          });
-          numIndex++;
-          return num;
-        });
+				let numIndex = 0
+				const formattedCount = children.replace(/\d+([,.]\d+)?/g, () => {
+					const num = newStart[numIndex].toLocaleString(undefined, {
+						maximumFractionDigits: 0,
+					})
+					numIndex++
+					return num
+				})
 
-        setCount(formattedCount);
+				setCount(formattedCount)
 
-        if (newStart.every((num, index) => num >= numbers[index])) {
-          setCount(children);
-        } else {
-          setTimeout(() => step(newStart), incrementTime);
-        }
-      };
+				if (newStart.every((num, index) => num >= numbers[index])) {
+					setCount(children)
+				} else {
+					setTimeout(() => step(newStart), incrementTime)
+				}
+			}
 
-      step(startNumbers);
-    } else {
-      controls.start({ opacity: 0, y: -20 });
-      setCount(children);
-    }
-  }, [inView, controls, children]);
+			step(startNumbers)
+		} else {
+			controls.start({ opacity: 0, y: -20 })
+			setCount(children)
+		}
+	}, [inView, controls, children])
 
-  return (
-      <motion.span
-        ref={ref}
-        className={className}
-        initial={{ opacity: 0, y: -20 }}
-        animate={controls}
-      >
-        {count}
-      </motion.span>
-  );
+	return (
+		<motion.span
+			ref={ref}
+			className={className}
+			initial={{ opacity: 0, y: -20 }}
+			animate={controls}
+		>
+			{count}
+		</motion.span>
+	)
 }
--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,4 +1,4 @@
-export { Counter } from './counter';
-export { EmblaCarouselWrapper } from './embla_carousel';
-export { WhyUsCard } from './why_us_card';
-export { YachtsCard } from './yachts_card';
+export { Counter } from './counter'
+export { EmblaCarouselWrapper } from './embla_carousel'
+export { WhyUsCard } from './why_us_card'
+export { YachtsCard } from './yachts_card'
