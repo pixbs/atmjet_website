81d260c290d0c40cf86f37be170de0407b4787f0
pixbs <hey@pixbs.com>
2024-07-15 06:29:34 +0200

    ✅ feat: Update CounterInput component and form validation

--- src/components/elements/counter_input.tsx
+++ src/components/elements/counter_input.tsx
@@ -1,54 +1,79 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useController, useFormContext } from 'react-hook-form'
 
 interface CounterInputProps {
-	placeholder: string
+	label: string
+	id: string
 }
 
 export function CounterInput(props: CounterInputProps) {
-	const { placeholder } = props
-	const [count, setCount] = useState(1)
+	const { control } = useFormContext()
+	const {
+		field: { value, onChange, onBlur },
+	} = useController({
+		name: props.id,
+		control,
+		defaultValue: 1,
+	})
+
+	const { label, id } = props
+
+	// Sync internal state with the form state
+	useEffect(() => {
+		setCount(value)
+	}, [value])
+
+	const [count, setCount] = useState(value)
 
 	const increment = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault()
 		if (count >= 25) {
 			setCount(25)
+			onChange(25)
 			return
 		}
-		setCount(count + 1)
+		const newCount = count + 1
+		setCount(newCount)
+		onChange(newCount)
 	}
 
 	const decrement = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault()
 		if (count <= 1) {
 			setCount(1)
+			onChange(1)
 			return
 		}
-		setCount(count - 1)
+		const newCount = count - 1
+		setCount(newCount)
+		onChange(newCount)
 	}
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		e.preventDefault()
-		// Assuming e.target.value is a string, convert it to a number first
 		const value = Number(e.target.value)
 		if (Number.isNaN(value)) {
 			return
 		}
 		if (value < 1) {
 			setCount(1)
+			onChange(1)
 			return
 		}
 		if (value > 25) {
 			setCount(25)
+			onChange(25)
 			return
 		}
 		setCount(value)
+		onChange(value)
 	}
 
 	return (
 		<div className="flex-row items-center rounded-lg bg-gray-900 px-4 py-2.5 text-sm font-normal text-gray-100">
-			<label className="pr-4">{placeholder}</label>
+			<label className="pr-4">{label}</label>
 			<button
 				className={`size-8 rounded-lg border border-gray-700 p-0 ${count <= 1 ? 'cursor-not-allowed opacity-50' : ''}`}
 				children="-"
@@ -56,9 +81,9 @@ export function CounterInput(props: CounterInputProps) {
 			/>
 			<input
 				className="w-10 border-none bg-transparent stroke-none p-0 py-1 text-center focus:stroke-none"
-				defaultValue={count}
 				onChange={handleChange}
 				value={count}
+				onBlur={onBlur}
 			/>
 			<button
 				className={`size-8 rounded-lg border border-gray-700 p-0 ${count >= 25 ? 'cursor-not-allowed opacity-50' : ''}`}
--- src/components/form/request_flight.tsx
+++ src/components/form/request_flight.tsx
@@ -1,17 +1,67 @@
+'use client'
+
 import { useTranslations } from 'next-intl'
 import { CounterInput } from '../elements'
+import { z } from 'zod'
+import { useForm, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+	from: z.string().nonempty('From is required'),
+	to: z.string().nonempty('To is required'),
+	date: z.string().nonempty('Date is required').refine((val) => !isNaN(Date.parse(val)), { message: "Invalid date format" }),
+	passangers: z.number().min(1, 'At least one passenger is required'),
+})
 
 export function RequestForm() {
 	const t = useTranslations('form')
+	type FormData = z.infer<typeof schema>
+	const methods = useForm<FormData>({
+		resolver: zodResolver(schema),
+	})
+
+	const { register, handleSubmit } = methods
+
+	const onSubmit = (data: FormData) => {
+		console.log(data)
+	}
+
+	const handleClick = () => {
+		const input = document.querySelector('input[type="date"]') as HTMLInputElement;
+		if (input) {
+			input.focus();
+		}
+	}
+
 	return (
-		<form className="flex flex-col gap-1">
-			<input placeholder={t('from')}></input>
-			<input placeholder={t('to')}></input>
-			<input type="date" placeholder={t('date')} />
-			<CounterInput placeholder={t('passangers')} />
-			<button type="submit" className="big mt-3 bg-gold bg-fixed">
-				{t('request-quote')}
-			</button>
-		</form>
+		<FormProvider {...methods}>
+			<form className="flex flex-col gap-1" onSubmit={handleSubmit(onSubmit)}>
+				<input
+					placeholder={t('from')}
+					{...register('from')}
+				/>
+				<input
+					placeholder={t('to')}
+					{...register('to')}
+				/>
+				<div 
+					className='bg-gray-900 rounded-2xl'
+					onClick={handleClick}
+				>
+					<input
+						type="date"
+						placeholder={t('date')}
+						{...register('date')}
+					/>
+				</div>
+				<CounterInput
+					label={t('passangers')}
+					id="passangers"
+				/>
+				<button type="submit" className="big mt-3 bg-gold bg-fixed">
+					{t('request-quote')}
+				</button>
+			</form>
+		</FormProvider>
 	)
 }
