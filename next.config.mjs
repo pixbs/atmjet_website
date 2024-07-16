53e66ed289fe217aa6c7810a87af11872b555c6a
pixbs <hey@pixbs.com>
2024-07-17 00:50:21 +0200

    🐕 feat: Update webpack configuration to handle SVG files with @svgr/webpack 🖼️ feat: Add images to the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,33 @@
-import createNextIntlPlugin from 'next-intl/plugin'
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test('.svg'),
+    );
 
-export default withNextIntl(nextConfig)
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+          use: ['@svgr/webpack'],
+        },
+      );
+
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
+};
+
+export default withNextIntl(nextConfig);
