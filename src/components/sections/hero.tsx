b8dff590db9339ec0ca8a1637a093ec78c7e07d6
Dimm <hey@pixbs.com>
2024-07-17 18:23:18 +0200

    🗑️ chores: Update hero section bg

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,7 +7,7 @@ export function HeroSection(props: HeroSectionProps) {
 	const { title } = props
 	const videoSrc = 'video/hero_background.mp4'
 	return (
-		<section className='bg-blue-500'>
+		<section>
 			<div className='container z-20 !my-0 h-svh justify-center gap-2'>
 				<p className='text-center text-sm'>ATMJET</p>
 				<h1 className='text-center duration-1000 animate-in fade-in slide-in-from-top-10'>
