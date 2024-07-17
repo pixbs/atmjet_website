73fccdb9a24d1568c2038241e39cf26a018cd18f
Dimm <hey@pixbs.com>
2024-07-17 17:49:40 +0200

    🎥 feat: Add video background to hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,21 +1,35 @@
 'use client'
+
+import Video from 'next-video'
+
 interface HeroSectionProps {
 	title: string
 }
 
 export function HeroSection(props: HeroSectionProps) {
 	const { title } = props
+	const videoSrc = 'video/hero_background.mp4'
 	return (
 		<section className='bg-blue-500'>
-			<div className='container z-10 !my-0 h-svh justify-center gap-2'>
+			<div className='container z-20 !my-0 h-svh justify-center gap-2'>
 				<p className='text-center text-sm'>ATMJET</p>
 				<h1 className='text-center duration-1000 animate-in fade-in slide-in-from-top-10'>
 					{title}
 				</h1>
-				<p className='absolute bottom-8 left-5 text-sm'>©ATM JET</p>
+				<p className='absolute bottom-8 left-5 z-20 text-sm'>©ATM JET</p>
 				{/* <RequestForm /> */}
 			</div>
-			<div className='hero-darkening absolute inset-0' />
+			<div className='hero-darkening absolute inset-0 z-10' />
+			<video
+				autoPlay
+				muted
+				loop
+				playsInline
+				className='absolute inset-0 z-0 h-full w-full object-cover'
+			>
+				<source src={videoSrc} type='video/mp4' />
+				<track src='/path/to/captions.vtt' kind='subtitles' srcLang='en' label='English' />
+			</video>
 		</section>
 	)
 }
