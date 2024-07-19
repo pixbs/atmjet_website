ef0e3c42a893fce455d3bc15bf668907b1178ed6
pixbs <hey@pixbs.com>
2024-07-19 08:58:25 +0200

    🪟 feat: Add booking modal functionality to contact us section

--- src/components/sections/contact_us.tsx
+++ src/components/sections/contact_us.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useState } from 'react'
+import { BookingModal } from './booking_modal'
 
 interface ContactUsSectionProps {
 	title: string
@@ -8,8 +10,19 @@ interface ContactUsSectionProps {
 }
 
 export function ContactUsSection(props: ContactUsSectionProps) {
+    const [isOpened, setIsOpened] = useState(false)
 	const { title, description, buttonText } = props
-	return (
+
+    const handleOpen = () => {
+		setIsOpened(true)
+		isOpened ? (document.body.style.overflow = 'auto') : (document.body.style.overflow = 'hidden')
+	}
+
+	const handleClose = () => {
+		setIsOpened(false)
+		document.body.style.overflow = 'auto'
+	}
+	return <>
 		<section
 			className='bg-cover bg-fixed bg-center'
 			style={{ backgroundImage: `url(${props.imageSrc})` }}
@@ -18,8 +31,14 @@ export function ContactUsSection(props: ContactUsSectionProps) {
 			<div className='container min-h-svh content-center items-center justify-center lg:min-h-[50svh]'>
 				<h2>{title}</h2>
 				<p className='pt-4'>{description}</p>
-				<button className='big mt-10'>{buttonText}</button>
+				<button 
+                    className='big mt-10'
+                    onClick={handleOpen}
+                >
+                    {buttonText}
+                </button>
 			</div>
 		</section>
-	)
+        {isOpened && <BookingModal closeModal={handleClose} />}
+    </>
 }
