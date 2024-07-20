3ddb4e25d476540b021ae69b1324c73784d9e28d
xmnlz <lemmeq9@gmail.com>
2024-07-20 14:30:20 +0200

    🐣 feat: Refactor drizzle-ssr component and update function name

--- src/components/drizzle-ssr.tsx
+++ src/components/drizzle-ssr.tsx
@@ -1,7 +1,9 @@
+'use server'
+
 import { db } from '@/lib/drizzle'
 import { airports } from '../lib/drizzle'
 
-export default async function drizzleSSR() {
+export default async function DrizzleSSR() {
 	const airportsList = await db.select().from(airports)
 
 	return <div>{airportsList.map((airport) => airport.id)}</div>
