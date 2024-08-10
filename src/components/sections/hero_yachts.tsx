8233d93d7730dae5488ac8d7a9eaf0fab455cfc6
pixbs <hey@pixbs.com>
2024-08-10 04:42:11 +0200

    🚢 feat: Update button text and add min-height to yachts hero section

--- src/components/sections/hero_yachts.tsx
+++ src/components/sections/hero_yachts.tsx
@@ -5,8 +5,8 @@ export function HeroYachtsSection() {
 	const t = useTranslations('yachts-hero')
 
 	return (
-		<section>
-			<div className='container z-20 !my-0 h-svh items-start justify-center gap-2'>
+		<section className='min-h-[680]'>
+			<div className='container z-20 !my-0 h-svh items-start justify-end pb-14 gap-2'>
 				<p className='text-sm uppercase'>{t('overline')}</p>
 				<h1 className='duration-1000 animate-in fade-in slide-in-from-top-10'>{t('title')}</h1>
 				<p className='max-w-lg pt-4 text-gray-900'>{t('description')}</p>
@@ -14,7 +14,7 @@ export function HeroYachtsSection() {
 				<Link href='?showBooking=Hero_yachts' scroll={false}>
 					<button className='big mt-8'>{t('button')}</button>
 				</Link>
-				<p className='absolute bottom-8 left-5 z-20 text-sm'>©ATM JET</p>
+				{/* <p className='absolute bottom-8 left-5 z-20 text-sm'>©ATM JET</p> */}
 			</div>
 			<div className='hero-darkening absolute inset-0 z-10' />
 			<div
