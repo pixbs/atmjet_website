fc8da18bec2a1de1cb943c502495ce71153e04a9
Dimm <hey@pixbs.com>
2024-08-14 17:10:26 +0200

    🏞️ feat: Removed title and gradient from yacht card image carousel

--- src/components/elements/yacht_card.tsx
+++ src/components/elements/yacht_card.tsx
@@ -11,14 +11,17 @@ export function YachtCard(props: typeof yachts.$inferSelect) {
 	return (
 		<div className='relative w-full shrink-0 gap-6 pr-4 md:gap-10 lg:flex-row'>
 			<div className='relative w-full overflow-hidden rounded-xl'>
-				<h3 className='absolute inset-8 z-20'>{name}</h3>
+				
 				<ImagesCarousel slides={props.pictures || []} />
-				{/* <div
+				{/* 
+				<h3 className='absolute inset-8 z-20'>{name}</h3>
+				<div
 					className='aspect-video rounded-xl bg-cover bg-center'
 					style={{ backgroundImage: `url(${props.pictures && props.pictures[0]})` }}
 				/>
-				*/}
 				<div className='absolute inset-0 z-10 bg-gradient-to-b from-gray-150 via-gray-150/20 to-transparent'></div>
+				*/}
+				
 			</div>
 			<div className='flex-row justify-around lg:flex-col'>
 				<Numeric title='Guests:' number={props.guests || 0}>
