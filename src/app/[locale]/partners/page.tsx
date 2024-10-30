a4f7d576a75487af40a1721ad7a81240578c0071
Dimm <hey@pixbs.com>
2024-10-30 16:27:20 +0100

    🌿 feat: Update partners

--- src/app/[locale]/partners/page.tsx
+++ src/app/[locale]/partners/page.tsx
@@ -1,19 +1,32 @@
 import { Counter } from '@/components/elements'
 import { PersonalManagerSection, WhyUsSection } from '@/components/sections'
 import NewContactUs from '@/components/sections/new_contact_us'
-import { useTranslations } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 import Image from 'next/image'
 
 export default function PartnersPage() {
 	const t = useTranslations()
+	const locale = useLocale()
 
-	const whyUsCards = ['card1', 'card2', 'card3', 'card4'].map((card) => ({
+	const weOfferCards = ['card1', 'card2', 'card3', 'card4'].map((card) => ({
 		num: '',
 		title: t(`we-offer.${card}.title`),
 		description: t(`we-offer.${card}.description`),
 	}))
 
+	const whyUsCards = ['card1', 'card2', 'card3', 'card4', 'card5'].map((card) => ({
+		num: t(`home-why-us.${card}.num`),
+		title: t(`home-why-us.${card}.title`),
+		description: t(`home-why-us.${card}.description`),
+	}))
 	const whyUsImages = [
+		'/images/home_page/why_us_years.webp',
+		'/images/home_page/why_us_clients.webp',
+		'/images/home_page/why_us_trusted_by_celeb.webp',
+		'/images/home_page/why_us_same_day_departure.webp',
+		'/images/home_page/why_us_excellence.webp',
+	]
+	const weOfferImages = [
 		'/images/home_page/why_us_years.webp',
 		'/images/home_page/why_us_clients.webp',
 		'/images/home_page/why_us_trusted_by_celeb.webp',
@@ -43,17 +56,23 @@ export default function PartnersPage() {
 						width={1200}
 						height={260}
 					/>
-					<div className='card gap-4 bg-gray-150 p-8 md:p-10'>
+					{/* <div className='card gap-4 bg-gray-150 p-8 md:p-10'>
 						<h2>{t('instant-payment.title')}</h2>
 						<p>{t('instant-payment.description')}</p>
-					</div>
+					</div> */}
 				</div>
 			</section>
 			<WhyUsSection
-				title={t('we-offer.subtitle')}
-				description={t('we-offer.description')}
+				title={locale === 'en' ? 'Clients benefit' : 'Клиенты выбирают нас'}
+				description=''
 				cards={whyUsCards}
 				images={whyUsImages}
+			/>
+			<WhyUsSection
+				title={t('we-offer.subtitle')}
+				description={t('we-offer.description')}
+				cards={weOfferCards}
+				images={weOfferImages}
 			/>
 			<PersonalManagerSection />
 			<NewContactUs />
