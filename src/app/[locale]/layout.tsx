bf85a99e7d4a81b34a02be7983f4a3cad0ce8631
pixbs <hey@pixbs.com>
2024-11-12 23:19:30 +0100

    🐜 feat: Add metadata & change sales-dept link name

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,19 +2,25 @@ import { BookingDialog } from '@/components/elements'
 import { Cookies, FooterSection, HeaderSection } from '@/components/sections'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import type { Metadata } from 'next'
 import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
+import { getLocale, getMessages } from 'next-intl/server'
 import { Inter } from 'next/font/google'
 import { headers } from 'next/headers'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-	title: 'ATM JET - Flying private made simple',
-	description:
-		'Renting a plane is the best solution for those who value comfort and time. ATM JET provides the opportunity to order a private plane for a flight to any point on the planet. We have been working for you for over 16 years and have vast experience in organizing business aviation flights.',
+export async function generateMetadata() {
+	const locale = await getLocale()
+	const baseURL =
+		process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL || 'localhost:3000'
+
+	const title = 'ATM JET - Flying private made simple'
+	const description = locale === 'ru' ? 'Аренда самолета - лучшее решение для тех, кто ценит комфорт и время. ATM JET предоставляет возможность заказать частный самолет для перелета в любую точку планеты. Мы работаем для вас уже более 16 лет и имеем огромный опыт в организации полетов бизнес-авиации.' : 'Renting a plane is the best solution for those who value comfort and time. ATM JET provides the opportunity to order a private plane for a flight to any point on the planet. We have been working for you for over 16 years and have vast experience in organizing business aviation flights.'
+	const metadata = {
+		title: title,
+		description: description,
+	}
 }
 
 export default async function LocaleLayout({
